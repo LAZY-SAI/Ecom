795e5a37e0afa658e0e43720de7de661212fe2ed
LAZY-SAI <mgrrahul200@gmail.com>
2025-06-14 17:00:41 +0545

    ..

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import Home from './Pages/Home';
 import AddProduct from './Pages/AppProduct';
 import EditProduct from './Pages/EditProduct';
 import './App.css'
